e78c6a37ea556d5ae2ed09c28a385abefa603652
moufidayoub11 <136031684+moufidayoub11@users.noreply.github.com>
2023-08-20 14:29:44 +0100

    Task 0. I'm not going anywhere. You can print that wherever you want to. I'm here and I'm a Spur for life

--- printf_helper.c
+++ printf_helper.c
@@ -7,12 +7,12 @@
  * @currentIndex: the current index in the foramt
  * @argList: the list of arguments to be printed.
  *
- * Return: number of printed chars, -1 if error
+ * Return: number of printed chars, -1 if error, unknow_len if neither
  */
 
 int printf_helper(const char *format, int currentIndex, va_list argList)
 {
-	int i;
+	int i, unknow_len = 0;
 
 	fs_t fs_types[] = {
 		{'c', print_character}, {'s', print_string}, {'%', print_percent_sign},
@@ -23,8 +23,15 @@ int printf_helper(const char *format, int currentIndex, va_list argList)
 			return (fs_types[i].action(argList));
 
 	if (fs_types[i].specifier == '\0')
+	{
 		if (format[currentIndex] == '\0')
 			return (-1);
+		unknow_len += write(1, "%%", 1);
+		if (format[currentIndex - 1] == ' ')
+			unknow_len += write(1, " ", 1);
+		unknow_len += write(1, &format[currentIndex], 1);
+		return (unknow_len);
+	}
 
 	return (-1);
 }
