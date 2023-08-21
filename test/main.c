9bcce16a94d3f6bfe90b907df01760c858b8aa05
moufidayoub11 <136031684+moufidayoub11@users.noreply.github.com>
2023-08-21 17:02:11 +0100

    New test file

--- test/main.c
+++ test/main.c
@@ -38,5 +38,7 @@ int main(void)
     len2 = printf("Percent:[%%]\n");
     _printf("Len:[%d]\n", len);
     printf("Len:[%d]\n", len2);
+    _printf("Unknown:[%l]\n");
+    printf("Unknown:[%l]\n");
     return (0);
 }
